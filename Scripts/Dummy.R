e274c289c50c7984f76ad78e77b5e732e48dc0d7
Andrey Alexandre <andreyalexandre16@gmail.com>
2019-10-28 10:40:55 -0300

    Iniciadas as tentativas de fazer as 1000 simulações

--- Scripts/Dummy.R
+++ Scripts/Dummy.R
@@ -1,112 +1,104 @@
-#Note ANDREY
-folder  <-  "C:/Users/Andrey/Projetos/PerLog/Scripts/"
-# PC TRABALHO
-#folder <- 'C:/Users/Andrey/Documents/Andrey/TCC/'
-# PC Casa Alessandro
-#folder <- '/home/alessandro/Dropbox/alessandro/2018_2/Orientacao_Monografia_Katiuski/TCCKatiuski/Monografia/codigo/'
-source(paste(folder, 'funcoes.txt', sep = ''))
+resp <- y; covar <- X
+li <- 0.000000001; ls <- 0.999999999
+n <- length(resp); k <- dim(covar)[2]
+par <- c(beta, rho)
+beta.vet  <-  par[1:k]
+# rho  <-  exp(par[k+1])
+rho  <-  par[(k+1):length(par)]
+s <- length(rho)
+per <- ifelse(2:n%%s == 0, s, 2:n%%s)
 
-LOOP <- expand.grid(n = c(100, 400), rho1 = c(.8, -.5), rho1 = c(-.4, .6), rho1 = c(.8, -.5))
-function(PAR){
-  # simulacao
-  n <- PAR[1]; k <- 5; rho <- unlist(PAR[-1]); beta <- rep(0.5, k);n.desc <- 50;m <- n+n.desc; pfn.target <- .05
-  X1 <- cbind(1, matrix(runif(n = (m*(k-1)), min = -1, max = 1), m, (k-1)))
-  colnames(X1) <- c('Intercept', kronecker("X_", 1:(k-1), paste, sep=''))
-  X <- X1[1:n,]; Xprev <-  X1[(n+1):m,]
-  REP <- 1000
-  
-  est_glm <- yhat_glm <- std_glm <- est_MC <- std_MC <- fit_glmvet <- corte0 <- corte1 <- corte <- NULL
-  pfp.insample.MC <- pfp.insample.glm <- pfn.insample.MC <- pfn.insample.glm <- NULL
-  pfp.glm <- pfn.glm <- pfp.MC <- pfn.MC <- NULL
-  fit_glmvet <- fit_MCvet <- list()
-  est_glm <- std_glm <- matrix(nrow = REP, ncol = k)
-  est_MC <- std_MC <- matrix(nrow = REP, ncol = (k+length(rho)))
-  Resultados <- list()
-  
-  r <- 1
-  
-  ct.fim.vet <- prob <-  fit_MCvet <- y.desc <- list()
-  set.seed(555)
-  while(r <= REP){
-    cat('Repl.', r)
-    ytotal <- gen(X = X1, beta = beta, rho = rho)
-    y.desc[[r]] <- ytotal[(n+1):m]
-    y <- ytotal[1:n]
-    #lassim  <-  cv.glmnet(x=matr, y=O3, family = 'binomial', alpha=1, standardize = TRUE)
-    fit_glm1 <-  glm(y~X-1, family = 'binomial')
-    fit_glmvet[[r]] <-  (fit_glm1)
-    fit_glm <-  summary(fit_glm1)$coefficients
-    #lambda <-  rbind(lambda, lassim$lambda)
-    yhat_glm <- rbind(yhat_glm,fitted(fit_glm1))
-    # fit_MC <- logistic_MC(resp = y, covar = X, par0 = c(beta, rho), trace = 0)
-    fit_MC <- tryCatch(logistic_MC(resp = y, covar = X, par0 = c(beta, rho), trace = 0),
-                       error = function(e){NULL})
-    fit_MCvet[[r]] <- fit_MC
-    if(!is.null(fit_MC)){
-      est_glm[r,] <- fit_glm[,1]
-      std_glm[r,] <- fit_glm[,2]
-      est_MC[r,] <- fit_MC[,1]
-      std_MC[r,] <- fit_MC[,2]
-      
-      prob[[r]] <- predi(beta.vet=fit_MCvet[[r]][1:k,1],
-                         rho = fit_MCvet[[r]][(1:length(rho)+k),1], covar=X, resp=y)
-      
-      prob.glm  <-   predict(object = fit_glmvet[[r]], newx = X, type=c("response"))
-      prob.MC  <-  prob[[r]]
-      in.sample.glm <- min.pfp.glm(y = y, prob = prob.glm, pfn.target = pfn.target)
-      pfp.insample.glm[r] <- in.sample.glm$pfp.min
-      pfn.insample.glm[r] <- in.sample.glm$pfn.min
-      corte[r] <- in.sample.glm$cortemin
-      in.sample.MC <- min.pfp.MC(y = y, prob = prob.MC, pfn.target = pfn.target)
-      pfp.insample.MC[r] <- in.sample.MC$pfp.min
-      pfn.insample.MC[r] <- in.sample.MC$pfn.min
-      corte0[r] <- in.sample.MC$corte0min
-      corte1[r] <- in.sample.MC$corte1min
-      
-      pr.glm <- prev.glm(fit = fit_glmvet[[r]], newY = y.desc[[r]], newX = Xprev,
-                         corte = corte[r])
-      pfp.glm[r] <- pr.glm$pfp; pfn.glm[r] <- pr.glm$pfn
-      pr.MC <- prev.MC(beta = fit_MCvet[[r]][1:k,1],
-                       rho  = fit_MCvet[[r]][(k+length(rho)),1],
-                       newY = y.desc[[r]], newX = Xprev,
-                       LastY= y[n], LastX = X[n,], corte0 = corte0[r], corte1 = corte1[r])
-      pfp.MC[r] <- pr.MC$pfp; pfn.MC[r] <- pr.MC$pfn
-      
-      r  <-  r+1
-      cat('\n')
-    }
-    else{
-      cat('Deu ruim!\n')
-    }
-  }
-  
-  
-  pf_insample_mat <- rbind(c(mean(pfp.insample.glm), mean(pfp.insample.MC)),
-                           c(mean(pfn.insample.glm), mean(pfn.insample.MC)))
-  pf_out_mat <- rbind(c(mean(pfp.glm), mean(pfp.MC)),
-                      c(mean(pfn.glm), mean(pfn.MC)))
-  pf_mat <- rbind(pf_insample_mat, pf_out_mat)
-  rownames(pf_mat) <- c('PFP - insample','PFN - insample', 'PFP - out','PFN - out')
-  colnames(pf_mat) <- c('GLM','MC')
-  
-  Bias <- cbind(c(colMeans(est_glm)-beta, rep(NA, length(rho))), (colMeans(est_MC, na.rm = T)-c(beta, rho)))
-  EP <- cbind(c(sqrt(colMeans(est_glm^2)-colMeans(est_glm)^2), rep(NA, length(rho))), sqrt(colMeans(est_MC^2, na.rm = T)-colMeans(est_MC, na.rm = T)^2))
-  RMSE <- sqrt(Bias^2+EP^2)
-  mean.std <- cbind(c(colMeans(std_glm), rep(NA, length(rho))), colMeans(std_MC, na.rm = T))
-  rownames(Bias) <- rownames(EP) <- rownames(RMSE) <- rownames(mean.std) <- names(colMeans(est_MC))
-  mat_Bias_RMSE <- cbind(Bias, RMSE); colnames(mat_Bias_RMSE) <- c('Bias - GLM','Bias - MC', 'RMSE - GLM','RMSE - MC')
-  
-  mat.EP <- cbind(EP[,1], mean.std[,1], EP[,2], mean.std[,2])
-  colnames(mat.EP) <- c('Emp. EP - GLM', 'Hes. EP - GLM', 'Emp. EP - MC', 'Hes. EP - MC')
-  
-  ### RESULTADOS PARA GUARDAR
-  resultado <- list('A' = list('PFN - In Sample - GLM' = sum(pfn.insample.glm>.05)/REP,
-                               'PFN - In Sample - GLM' = sum(pfn.insample.MC>.05)/REP, 
-                               'PF - Matriz' = pf_mat,
-                               'Bias e RMSE' = mat_Bias_RMSE,
-                               'Matriz de EP' = mat.EP ))
-  names(resultado) <- paste('n = ', n, " e rho's = (", paste(rho, collapse = ', '), ")")
-  Resultados <- append(Resultados, resultado)
-  saveRDS(object = Resultados, file = "C:/Users/Andrey/Projetos/PerLog/Dados/Resultados.rds")
+p <- NULL
+
+theta <- exp(covar%*%beta.vet)/(1+exp(covar%*%beta.vet)); theta <- as.vector(theta)
+theta[theta<=li] <- li;theta[theta>=ls] <- ls
+
+p[1]  <-  theta[1]
+
+c1  <-  theta[2:n]*(1-theta[2:n])
+c1aux  <-  theta[2:n-1]*(1-theta[2:n-1]); c1aux <- c(0, c1aux)
+c2  <-  theta[2:n-1]/(1-theta[2:n-1])
+c3  <-  (1-theta[2:n-1])/theta[2:n-1]
+c4  <-  (theta[2:n-1]^(-.5))*((1-theta[2:n-1])^(-1.5))
+c5  <-  (theta[2:n-1]^(-1.5))*((1-theta[2:n-1])^(-.5))
+
+# c1[c1==-Inf] <- -999999999999999999999999;c1[c1==Inf] <- 999999999999999999999999
+# c2[c2==-Inf] <- -999999999999999999999999;c2[c2==Inf] <- 999999999999999999999999
+# c3[c3==-Inf] <- -999999999999999999999999;c3[c3==Inf] <- 999999999999999999999999
+# c4[c4==-Inf] <- -999999999999999999999999;c4[c4==Inf] <- 999999999999999999999999
+# c5[c5==-Inf] <- -999999999999999999999999;c5[c5==Inf] <- 999999999999999999999999
+
+p[2:n]  <-  theta[2:n] - rho[per]*sqrt(c1)*(sqrt(c2)-resp[2:n-1]*(sqrt(c2)+sqrt(c3)))
+
+p[p<=li] <- li;p[p>=ls] <- ls
+
+dpdvt0  <-  1-.5*rho[per]*(c1^(-.5))*(1-2*theta[2:n])*(sqrt(c2)-resp[2:n-1]*(sqrt(c2)+sqrt(c3)))
+dpdvt1  <-  .5*rho[per]*sqrt(c1)*(c4-resp[2:n-1]*(c4-c5))
+dpdvt0  <-  c(1,dpdvt0)
+dpdvt1  <-  c(0,dpdvt1)
+
+dldp  <-  (resp-p)/(p*(1-p))
+dvt0db  <-  theta*(1-theta)*covar
+dvt1db  <-  c1aux*covar
+
+dpdb  <-  dpdvt0*dvt0db + dpdvt1*dvt1db
+dldb  <-  as.vector(dldp)*dpdb
+
+dpdrho  <-  -sqrt(c1)*(sqrt(c2)-resp[2:n-1]*(sqrt(c2)+sqrt(c3))); dpdrho  <-  c(0, dpdrho)
+dpdrho <- permatrix(vector = dpdrho, period_vec = per, period = s)
+dldrho  <-  dldp*dpdrho
+
+# gradient
+grad  <-  c(colSums(dldb), colSums(dldrho))
+
+#hessian
+d2ldp2  <-  -resp/(p^2)-(1-resp)/((1-p)^2)
+d2ldrho2  <-  array(0, dim = c(s, s, n))
+for (t in 1:n) {
+  d2ldrho2[ , ,t]  <-  d2ldp2[t]*dpdrho[t,]%*%t(dpdrho[t,])
 }
 
+d2ldb2  <- array(0, dim = c(k, k, n))
+for (t in 1:n) {
+  d2ldb2[ , , t]  <- d2ldp2[t]*dpdb[t,]%*%t(dpdb[t,])
+}
+
+d2pdrhodvt0  <-  -.5*(c1^(-.5))*(1-2*theta[2:n])*(sqrt(c2)-resp[2:n-1]*(sqrt(c2)+sqrt(c3))); d2pdrhodvt0  <-  c(0,d2pdrhodvt0);
+d2pdrhodvt0 <- permatrix(vector = d2pdrhodvt0, period_vec = per, period = s)
+d2pdrhodvt1  <-  .5*sqrt(c1)*(c4-resp[2:n-1]*(c4-c5)); d2pdrhodvt1  <-  c(0,d2pdrhodvt1);
+d2pdrhodvt1 <- permatrix(vector = d2pdrhodvt1, period_vec = per, period = s)
+
+d2pdbdrho  <- array(0, dim = c(k, s, n))
+for (t in 1:n) {
+  d2pdbdrho[ , , t]  <- t(dvt0db)[,t, drop = F]%*%d2pdrhodvt0[t, , drop = F] + t(dvt1db)[,t, drop = F]%*% d2pdrhodvt1[t, , drop = F]
+}
+
+d2ldbdrho  <-  matrix(0, k, s)
+for (t in 1:n) {
+  d2ldbdrho  <-  d2ldbdrho + as.vector(d2ldp2)[t] * t(dpdb)[, t, drop = F] %*%  dpdrho[t, , drop = F] + as.vector(dldp)[t]*d2pdbdrho[, , t]
+}
+
+hess  <-  cbind(rbind(rowSums(x = d2ldb2, dims = 2), t(d2ldbdrho)), 
+                rbind( d2ldbdrho, rowSums(x = d2ldrho2, dims = 2)))
+
+diag( solve( -hess ) )
+
+
+
+
+out <- list(covar=covar, resp=resp, li=li, ls=ls)
+otim  <-  optim(par = par, fn = loglik, gr = gr.loglik, out=out, method = "L-BFGS-B",
+                lower = c(rep(-Inf, dim(covar)[2]), -0.999999),
+                upper = c(rep(Inf, dim(covar)[2]), 0.999999), hessian = T,
+                control = list(trace=0))
+
+diag(solve(hess.loglik(par = otim$par, out = out)))
+diag( solve( -hess ) )
+diag( solve( otim$hessian ) )
+# seotim <- sqrt(diag(solve(otim$hessian)))
+seotim  <-  sqrt(diag(solve(hess.loglik(par = otim$par, out = out))))
+mat.res <- cbind(otim$par, seotim, 2*(1-pnorm(abs(otim$par/seotim))))
+colnames(mat.res) <- c('Estimate','Std.Error','p-value')
+# row.names(mat.res)[dim(covar)[2]+1] <- 'lambda'
+row.names(mat.res) <- c(colnames(covar), paste0('rho', seq(length(par0)-dim(covar)[2])))
+return(mat.res)
