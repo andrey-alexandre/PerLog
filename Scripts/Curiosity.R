18e2b8b55962375661990dc2a5b61c22706722ad
Andrey Alexandre <andreyalexandre16@gmail.com>
2020-02-21 06:16:44 -0300

    Alterado script de teste de hipotese

--- Scripts/Curiosity.R
+++ Scripts/Curiosity.R
@@ -1,4 +1,4 @@
-z <- function(x){
+z_confidence_interval <- function(x){
   ux <- mean(x); sdx <- sd(x); n <- length(x)
   norm <- qnorm(p = .025, mean = ux, sd = sdx)
   bound <- norm * sdx * sqrt(n)
@@ -6,26 +6,46 @@ z <- function(x){
   output <- list(lower = ux - bound, upper = ux + bound)
   return(output)
 }
-
-test <- function(x){
-  bound <- z(x)
+z_test <- function(x){
+  bound <- z_confidence_interval(x)
   
   y <- ifelse(between(x, bound$lower, bound$upper), 0, 1)
   return(y)
 }
 
-std_glm <- read.csv(file = paste0('/home/andrey/Projetos/PerLog/Scripts/Resultados/', arg[length(arg)], '/parcial_std_glm.csv'), stringsAsFactors = F, header = F)
-std_MC <- read.csv(file = paste0('/home/andrey/Projetos/PerLog/Scripts/Resultados/', arg[length(arg)], '/parcial_std_MC.csv'), stringsAsFactors = F, header = F)
-std_MCP <- read.csv(file = paste0('/home/andrey/Projetos/PerLog/Scripts/Resultados/', arg[length(arg)], '/parcial_std_MCP.csv'), stringsAsFactors = F, header = F)
+normal_bound <- function(x){
+  ux <- mean(x); sdx <- sd(x); n <- length(x)
+  norm <- qnorm(p = .025)
+  
+  output <- list(lower = norm, upper = -norm)
+  return(output)
+}
+bound_test <- function(x){
+  bound <- normal_bound(x)
+  
+  y <- ifelse(between(x, bound$lower, bound$upper), 0, 1)
+  return(y)
+}
+
+std_glm <- read.csv(file = paste0('/home/andrey/Projetos/PerLog/Scripts/Resultados/E/parcial_std_glm.csv'), stringsAsFactors = F, header = F)
+std_MC <- read.csv(file = paste0('/home/andrey/Projetos/PerLog/Scripts/Resultados/E/parcial_std_MC.csv'), stringsAsFactors = F, header = F)
+std_MCP <- read.csv(file = paste0('/home/andrey/Projetos/PerLog/Scripts/Resultados/E/parcial_std_MCP.csv'), stringsAsFactors = F, header = F)
 
 std_glm
-std_glm %>% apply(2, z)
-std_glm %>% apply(2, test) %>% colMeans
+std_glm %>% apply(2, z_confidence_interval)
+std_glm %>% apply(2, z_test) %>% colMeans
+std_glm %>% apply(2, normal_bound)
+std_glm %>% apply(2, bound_test) %>% colMeans
 
 std_MC
-std_MC %>% apply(2, z)
-std_MC %>% apply(2, test) %>% colMeans
+std_MC %>% apply(2, z_confidence_interval)
+std_MC %>% apply(2, z_test) %>% colMeans
+std_MC %>% apply(2, normal_bound)
+std_MC %>% apply(2, bound_test) %>% colMeans
 
 std_MCP
-std_MCP %>% apply(2, z)
-std_MCP %>% apply(2, test) %>% colMeans
+std_MCP %>% apply(2, z_confidence_interval)
+std_MCP %>% apply(2, z_test) %>% colMeans
+std_MCP %>% apply(2, normal_bound)
+std_MCP %>% apply(2, bound_test) %>% colMeans
+      
